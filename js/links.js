f344331133328bf5666c9e4ef99ff6393b96e1b6
CeH9 <doter.ua@gmail.com>
2016-09-24 17:20:38 +0300

    js links

---
+++ js/links.js
@@ -0,0 +1,7 @@
+var links = document.links;
+
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+   if (links[i].hostname != window.location.hostname) {
+       links[i].target = '_blank';
+   } 
+}
