50ed33c22697364079682978d451c97aa6be39a7
CeH9 <doter.ua@gmail.com>
2017-03-23 19:23:23 +0200

    velocity creation fix

--- src/core/textInjection/VelocityHelper.java
+++ src/core/textInjection/VelocityHelper.java
@@ -1,9 +1,11 @@
 package core.textInjection;
 
 import global.utils.Logger;
+import org.apache.commons.collections.ExtendedProperties;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
-import org.jetbrains.java.generate.velocity.VelocityFactory;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.log.SimpleLog4JLogSystem;
 
 import java.io.StringWriter;
 import java.util.Map;
@@ -16,7 +18,7 @@ public class VelocityHelper {
     public static String fromTemplate(String velocityTemplate, Map<String, String> mapVariables) {
         StringWriter stringWriter = new StringWriter();
         try {
-            VelocityEngine velocityEngine = VelocityFactory.getVelocityEngine();
+            VelocityEngine velocityEngine = getVelocityEngine();
 
             VelocityContext velocityContext = new VelocityContext();
             for (Map.Entry<String, String> entry : mapVariables.entrySet()) {
@@ -29,6 +31,30 @@ public class VelocityHelper {
             return null;
         }
         return stringWriter.getBuffer().toString();
+    }
+
+
+    //=================================================================
+    //  Engine
+    //=================================================================
+    private static class Holder {
+        private static final VelocityEngine engine = newVelocityEngine();
+    }
+
+    private static VelocityEngine newVelocityEngine() {
+        ExtendedProperties prop = new ExtendedProperties();
+        prop.addProperty(RuntimeConstants.RUNTIME_LOG_LOGSYSTEM_CLASS, SimpleLog4JLogSystem.class.getName());
+        prop.addProperty("runtime.log.logsystem.log4j.category", "GenerateToString");
+
+        VelocityEngine velocity = new VelocityEngine();
+        velocity.setExtendedProperties(prop);
+        velocity.init();
+
+        return velocity;
+    }
+
+    private static VelocityEngine getVelocityEngine() {
+        return Holder.engine;
     }
 
 }
