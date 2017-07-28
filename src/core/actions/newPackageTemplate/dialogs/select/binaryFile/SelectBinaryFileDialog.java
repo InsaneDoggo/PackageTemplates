2d8b15d831a3b4d40caf80c30be8ceee63cfc4e9
CeH9 <doter.ua@gmail.com>
2017-07-28 23:33:49 +0300

    Binary Files part 1.2

--- src/core/actions/newPackageTemplate/dialogs/select/binaryFile/SelectBinaryFileDialog.java
+++ src/core/actions/newPackageTemplate/dialogs/select/binaryFile/SelectBinaryFileDialog.java
@@ -4,11 +4,13 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.ui.EditorTextField;
+import com.intellij.ui.components.JBRadioButton;
 import com.intellij.ui.components.JBScrollPane;
 import core.actions.newPackageTemplate.dialogs.implement.ImplementDialog;
 import global.utils.file.FileReaderUtil;
 import global.utils.i18n.Localizer;
 import global.utils.templates.PackageTemplateHelper;
+import net.miginfocom.layout.CC;
 import net.miginfocom.layout.LC;
 import net.miginfocom.swing.MigLayout;
 
@@ -19,7 +21,6 @@ import java.awt.*;
  * Created by CeH9 on 24.06.2016.
  */
 public abstract class SelectBinaryFileDialog extends DialogWrapper implements SelectBinaryFileView {
-
 
     private SelectBinaryFilePresenter presenter;
     private Project project;
@@ -36,6 +37,7 @@ public abstract class SelectBinaryFileDialog extends DialogWrapper implements Se
     //=================================================================
     private JPanel panel;
     private TextFieldWithBrowseButton btnPath;
+    private ButtonGroup bgSourceMode;
 
     private void render() {
         JLabel jlDescription = new JLabel(Localizer.get("Description"));
@@ -43,6 +45,11 @@ public abstract class SelectBinaryFileDialog extends DialogWrapper implements Se
         btnPath = new TextFieldWithBrowseButton();
         btnPath.setText(PackageTemplateHelper.getRootDirPath());
         btnPath.addBrowseFolderListener(Localizer.get("title.SelectBinaryFile"), "", project, FileReaderUtil.getBinaryFileDescriptor());
+
+        JBRadioButton rbCopyToDir;
+        JBRadioButton rbUseOriginal;
+
+        panel.add(btnPath, new CC().wrap().pushX().growX());
     }
 
 
