04f4b1d546eae8e843765c3d1c118a4695d1ce0e
CeH9 <doter.ua@gmail.com>
2017-08-23 17:06:45 +0300

    CreateBinaryFileAction part 1

---
+++ src/core/actions/custom/undoable/CreateBinaryFileAction.java
@@ -0,0 +1,73 @@
+package core.actions.custom.undoable;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.IncorrectOperationException;
+import core.actions.custom.element.CreateElementBaseAction;
+import core.report.ReportHelper;
+import core.report.models.FailedActionReport;
+import global.Const;
+import global.models.BinaryFile;
+import global.utils.Logger;
+import global.utils.file.FileWriter;
+import global.utils.file.VFSHelper;
+
+public class CreateBinaryFileAction extends CreateElementBaseAction<BinaryFile, java.io.File> {
+
+    public CreateBinaryFileAction(BinaryFile file, Project project) {
+        super(project, file);
+    }
+
+    @Override
+    protected void createElement(String path) {
+        //fileResult =
+    }
+
+    @Override
+    protected java.io.File getDuplicateFile(String path) {
+        return new java.io.File(path + java.io.File.separator
+                + element.getName() + Const.FILE_EXTENSION_SEPARATOR + element.getExtension());
+    }
+
+    @Override
+    protected String elementToString() {
+        return String.format("%s.%s",
+                element.getName(),
+                element.getExtension()
+        );
+    }
+
+    @Override
+    protected java.io.File findExistingResultFile(java.io.File fileDuplicate) {
+        return VFSHelper.findPsiFileByPath(project, fileDuplicate.getPath());
+    }
+
+    @Override
+    protected boolean removeExistingElement(java.io.File fileDuplicate) {
+        PsiFile psiDuplicate = (PsiFile) findExistingResultFile(fileDuplicate);
+        if (psiDuplicate == null) {
+            return false;
+        }
+
+        try {
+            psiDuplicate.delete();
+            return true;
+        } catch (IncorrectOperationException e) {
+            Logger.logAndPrintStack("CreateFileFromTemplateAction " + e.getMessage(), e);
+            ReportHelper.putReport(new FailedActionReport(this, e.getMessage()));
+            return false;
+        }
+    }
+
+
+    //=================================================================
+    //  Utils
+    //=================================================================
+    @Override
+    public String toString() {
+        return String.format("Create BinaryFile:    %s",
+                element.getName()
+        );
+    }
+
+}
