b9ccc8c3c240b533bee5fb661b5071a130516b9e
CeH9 <doter.ua@gmail.com>
2019-01-27 18:51:53 +0200

    Tests: Added testdata dir; Added FooBar test

--- src/test/kotlin/demo/DemoTest.kt
+++ src/test/kotlin/demo/DemoTest.kt
@@ -4,17 +4,32 @@ import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCa
 
 class DemoTest : LightPlatformCodeInsightFixtureTestCase() {
 
+    override fun getTestDataPath() = "testdata/demo"
+
     override fun setUp() {
         super.setUp()
     }
 
-    fun testMy() {
-        myFixture.configureByText("DemoTest.txt", "Hello<caret>")
+    fun testDemoHello() {
+        myFixture.configureByFile("${input()}.txt")
         myFixture.type(" World!")
-        myFixture.checkResult("Hello World!")
+        myFixture.checkResultByFile("${output()}.txt")
+    }
+
+    fun testDemoFooBar() {
+        myFixture.configureByFile("${input()}.txt")
+        myFixture.type("Bar")
+        myFixture.checkResultByFile("${output()}.txt")
     }
 
     override fun tearDown() {
         super.tearDown()
     }
+
+
+    //==================================================================================================================
+    //  Utils
+    //==================================================================================================================
+    private fun input() = "input/${getTestName(false)}"
+    private fun output() = "output/${getTestName(false)}"
 }
